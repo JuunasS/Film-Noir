79c29e6c2668527c61b4f5308a7cf5c1d483952c
Jarppana <71658900+Jarppana@users.noreply.github.com>
2022-04-01 15:56:13 +0300

    Alkeellinen liikkumis- ja interactaus systeemi
    - tein skenen panttilainaamolle
    - sijoitin skeneen mallin ja tein navmesh asiat
    - skenessä pystyy nyt liikkumaan, ja napauttamaan kirjapinoa tiskillä
    - tällä hetkellä kirjapinon napauttaminen ei tee mitään muuta kuin antaa debug viestin
    - kehittelen interactausta vielä lisää

--- FilmNoir/Assets/Scripts/PlayerController.cs
+++ FilmNoir/Assets/Scripts/PlayerController.cs
@@ -6,9 +6,11 @@ using UnityEngine.AI;
 public class PlayerController : MonoBehaviour
 {
     NavMeshAgent agent;
+    bool clicked;           // bool jolla seurataan onko painettu interactablea
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        clicked = false;
     }
 
     // Update is called once per frame
@@ -22,7 +24,22 @@ public class PlayerController : MonoBehaviour
             {
                 agent.SetDestination(hit.point);                            //asetetaan sijainti navmesh agentille ja se liikkuu
 
+                if (hit.collider.CompareTag("Interactable"))
+                {
+                    clicked = true;
+                    Debug.Log("Olet klikannut interactablea, yay! et‰isyys ");
+
+                }
             }
         }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Interactable") && clicked)    // t‰ll‰ pidet‰‰n huoli ettei ohi k‰velless‰ voi osua interactableen ja vahingossa saada interaction
+        {
+            Debug.Log("Olet saapunut interactablen luo!");
+        }
+        clicked = false;
     }
 }
