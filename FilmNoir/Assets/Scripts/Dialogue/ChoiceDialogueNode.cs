370031c9ae6ba18b8662e1564ba7c858c34b238e
JuunasS <62592089+JuunasS@users.noreply.github.com>
2022-04-01 13:39:06 +0300

    Dialogue System Testing
    - Added Game Manager
    - Changes to dialogue box
    - Small changes to dialogue scripts

--- FilmNoir/Assets/Scripts/Dialogue/ChoiceDialogueNode.cs
+++ FilmNoir/Assets/Scripts/Dialogue/ChoiceDialogueNode.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BasicDialogueNode : MonoBehaviour
+public class ChoiceDialogueNode : MonoBehaviour
 {
+    [SerializeField]
+    private List<DialogueNode> nodes;
+
     // Start is called before the first frame update
     void Start()
     {
