d21232f2af3e0f1ea8e191f8a776c5d76f53f12d
PyryPK <101798792+PyryPK@users.noreply.github.com>
2022-04-19 02:01:24 +0300

    MiniMenu
    Save/Load functionality doesn't work

--- FilmNoir/Assets/Scripts/MenuScript.cs
+++ FilmNoir/Assets/Scripts/MenuScript.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class MenuScript : MonoBehaviour
 {
+    GameObject menu;
     // Start is called before the first frame update
     void Start()
     {
-        
+        menu = GameObject.Find("MiniMenu");
+        menu.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            menu.SetActive(!menu.activeInHierarchy);
+        }
     }
 
     public void NewGame()
@@ -25,5 +30,10 @@ public class MenuScript : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void TitleReturn()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
