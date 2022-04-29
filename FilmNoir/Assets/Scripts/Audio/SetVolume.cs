bedceb57c84c9d127368e1005cdd8c7838766e16
JuunasS <62592089+JuunasS@users.noreply.github.com>
2022-04-29 15:54:31 +0300

    Audio is now saved between scenes

--- FilmNoir/Assets/Scripts/Audio/SetVolume.cs
+++ FilmNoir/Assets/Scripts/Audio/SetVolume.cs
@@ -2,14 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SetVolume : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    public Slider volumeSlider;
+
+    void Start()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("MusicVolume", 0.75f);
+    }
 
     public void SetVolumeLevel(float sliderValue)
     {
         audioMixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("MusicVolume", sliderValue);
     }
 
 }
